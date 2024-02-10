b0d868146221c2f87fae2060b30638238117d6f7
maciek01 <maciek@kolesnik.org>
2024-02-10 16:57:05 -0500

    set manual autofocus - infinity

--- uav1/bin/cam-on.sh
+++ uav1/bin/cam-on.sh
@@ -1,1 +1,2 @@
+#libcamera-vid -v 0 -t 0 --lens-position=0.0 --autofocus-mode=manual --nopreview --rotation 180  --inline -o udp://192.168.3.6:3333
 libcamera-vid -v 0 -t 0 --nopreview --rotation 180  --inline -o udp://192.168.3.6:3333
